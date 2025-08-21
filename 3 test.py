0a66df3c9c72b2a0d7e4f488c4f79242867b1cfc
zinebnadak <zinebnadak04@outlook.com>
2025-08-21 19:55:37 +0300

    Initial commit

--- 3 test.py
+++ 3 test.py
@@ -1,1 +1,60 @@
-print ("testnr3")
+
+
+print(type(a))
+
+
+# type of a=123 is an integer, BUT b="123" is a string
+a=123
+b="123"
+
+print(type(a))
+print(type(b))
+
+# multiple assignment: name multiple variables simultaniously
+v,s=5,"hello"
+
+print(v)
+print(s)
+
+
+#Program that reads and writes. Input argument, use end=" to keep in line
+name =  input ("what is your name?")
+print("Hello", name,"!", end="")
+age = input(" How old are you?")
+print("Your name is", name,",and you are", age, "years old")
+
+#Program that reads and calculates. Gives me the integer number squared. OBS! ONLY Integer number work!
+answer = input ("write me a number:")
+x=int(answer)
+y=x*x
+print ("The square of the number", answer, "is",y)
+
+# if you want the program to be able to print both integer and float numbers use float
+answer = input ("write me a number:")
+x=float(answer)
+y=x*x
+print ("The square of the number", answer, "is",y)
+
+#use formated string litterals, called f-strings to get the calculated float number neater ex. 1.6. For these we use curly braces/placeholders {}."y" = what is going to be answered. After ":" comes a formatspecification telling us how it is going to be answered. Follow up with a dot "." ,then "2", means answer with two decimals. Lastly "f" stands for float number. Use d for Integer numbers or nothing at all.
+answer= input("write a number")
+x=float(answer)
+y=x*x
+print(f"The square of the number is {y:.2f}")
+
+#you can also replace {y:.2f} to {x*x:.2f} directly and skip a line for shorter code
+
+#adding a number before the dot tells you how many big the spaces is going to be printed in the answer
+print(f"The square of the number is {y:6.2f}")
+# program gives you 6 spaced of which only 4 are used to print numbers
+print(f"The square of the number is {y:06.2f}")
+# program gives number starting with 0 to fill up the given space
+
+#you can use multiple placeholders in one single f-sting
+i=3
+j=6
+print(f'i={i} and j={j}')
+
+#adding "=" after variable to
+print (f'{i=} and {j=}')
+
+
