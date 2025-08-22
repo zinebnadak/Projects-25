49d67ff129fb2c39004d164eb9d6a27f23b775fb
zinebnadak <zinebnadak04@outlook.com>
2025-08-22 19:11:27 +0300

    updated version 22.8

--- #2Mathquiz.py
+++ #2Mathquiz.py
@@ -1,1 +1,8 @@
+#Pre-chosen number pairs for the quiz
+problems =[(2,3),(5,7),(10,15)]
 
+score = 0 #To keep track of correct answers
+
+print ("Welcome to the Addition Quiz!\n")
+
+#Create a Loop through each problem
