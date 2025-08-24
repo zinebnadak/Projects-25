12b8c3535fe52ee6abb1cf2fe6de22756889ef1f
zinebnadak <zinebnadak04@outlook.com>
2025-08-24 12:33:06 +0300

    updated version 22.8

--- #1Rollthedice.py
+++ #1Rollthedice.py
@@ -1,17 +1,18 @@
 import random
-# ask the user
-while True:
-    answer = input("Do you want to roll the dice, YES or NO?")
 
-if answer.lower()== "yes":
-    dice1 = random.randint(1,6)
-    dice2 = random.randint(1,6)
-    print("Your rolled:",dice1, "and",dice2)
-    print("Thanks for playing!")
-    break
-elif answer.lower()=="no":
-    print("Okey then, have a nice day!")
-    break
-else:
-    print("Invalid choice! Answer yes or no! Please try again! ")
+while True:
+    answer = input("Do you want to roll the dice, YES or NO? ")
+
+    if answer.lower() == "yes":
+        dice1 = random.randint(1, 6)
+        dice2 = random.randint(1, 6)
+        print("You rolled:", dice1, "and", dice2)
+        print("Thanks for playing!")
+        break
+    elif answer.lower() == "no":
+        print("Okay then, have a nice day!")
+        break
+    else:
+        print("Invalid choice! Answer yes or no! Please try again!")
+
 
